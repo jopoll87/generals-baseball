266c43688243d1c7c3c7233be0f93ba230181eb1
jopoll87 <poll.jonathan87@gmail.com>
2025-05-22 16:34:15 -0600

    added email button and cpanel file to prevent 404 error

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,14 +1,19 @@
 import Header from './Header';
 
 const Contact = () => {
+  const handleClick = () => {
+    return (window.location.href = 'mailto:utahgeneralsbaseball@gmail.com');
+  };
+
   return (
     <>
       <Header />
       <div className='body'>
         <div className='contact-info'>
-          <h3>Email:</h3>
-          <p>utahgeneralsbaseball@gmail.com</p>
-          <h3>Phone #:</h3>
+          <button onClick={handleClick} className='form-button' style={{backgroundColor: '#ceb36e', }}>
+            Email us
+          </button>
+          <h4>Phone #:</h4>
           <p>(801) 686-8511</p>
         </div>
       </div>
