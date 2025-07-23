18a317f2fe6a44817ee202158e2989a5edb65182
Jon Poll <jon.poll@schools.utah.gov>
2025-07-23 08:29:05 -0600

    added the one sponsor we currently have

---
+++ src/data/SponsorsData.tsx
@@ -0,0 +1,15 @@
+import OrePac from '../assets/sponsors/OrePac.jpg'
+
+export type SponsorType = {
+  id: number,
+  name: string,
+  image: string,
+}
+
+export const SPONSORS = [
+  {
+    id: 1,
+    name: 'Ore Pac',
+    image: OrePac,
+  }
+]
