ab4867b3bf51fed9e7913e8cd4e1a0ac2cec224b
Jon Poll <jon.poll@schools.utah.gov>
2025-07-25 09:09:12 -0600

    undid the lazy load that did nothing

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,20 +5,15 @@ import Header from './Header';
 const Body = () => {
   return (
     <>
-        <Header />
-        <div className='body'>
+      <Header />
+      <div className='body'>
         <h1>Inaugural Season Fall 2025</h1>
 
-        <img id='main-logo'
-            src={StarsShield}
-            alt='Generals main logo'
-        />
+        <img id='main-logo' src={StarsShield} alt='Generals main logo' />
 
-        
         <h2>Upcoming Games</h2>
         <GameChanger />
-
-        </div>
+      </div>
     </>
   );
 };
