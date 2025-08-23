02532ff8d1f97cb5e88f17a7aeca123128c827a7
jopoll87 <poll.jonathan87@gmail.com>
2025-08-22 19:35:47 -0600

    Player Page -- updated with actual player photos. Also added new team photo to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,19 @@
-import StarsShield from '../assets/Logos/GeneralsShieldStars.png';
+// import StarsShield from '../assets/Logos/GeneralsShieldStars.png';
+import Team from '../assets/Logos/TeamBannerPhoto.png';
 import Header from './Header';
 
 const Body = () => {
   return (
     <>
-        <Header />
-        <div className='body'>
+      <Header />
+      <div className='body'>
         <h1>Inaugural Season Fall 2025</h1>
-        <img id='main-logo'
+        {/* <img id='main-logo'
             src={StarsShield}
             alt='Generals main logo'
-        />
-        </div>
+        /> */}
+        <img id='team-photo' src={Team} alt='Generals Team Photo' />
+      </div>
     </>
   );
 };
--- src/data/PlayerData.tsx
+++ src/data/PlayerData.tsx
@@ -1,4 +1,16 @@
-import DefaultPlayer from '../assets/players/PlayerStandin.jpg'
+import McCoy from '../assets/players/McCoyPoster.jpg'
+import Evan from '../assets/players/EvanPoster.jpg'
+import Easton from '../assets/players/EastonPoster.jpg'
+import Bron from '../assets/players/BronPoster.jpg'
+import Carter from '../assets/players/CarterPoster.jpg'
+import Connor from '../assets/players/ConnorPoster.jpg'
+import Ethan from '../assets/players/EthanPoster.jpg'
+import Holden from '../assets/players/HoldenPoster.jpg'
+import Jaxson from '../assets/players/JaxsonPoster.jpg'
+import Kelton from '../assets/players/KeltonPoster.jpg'
+import Peter from '../assets/players/PeterPoster.jpg'
+import Warner from '../assets/players/WarnerPoster.jpg'
+
 
 export type PlayerType = {
   id: number,
@@ -13,84 +25,84 @@ export const PLAYERS =[
     id: 1,
     name: 'McCoy',
     playerNumber: 15,
-    image: DefaultPlayer,
-    positions: ['Pitcher', 'Shortstop', 'Second']
+    image: McCoy,
+    positions: ['Pitcher', 'Shortstop', 'Second', 'Outfield']
   },
   {
     id: 2,
     name: 'Evan',
     playerNumber: 22,
-    image: DefaultPlayer,
+    image: Evan,
     positions: ['Pitcher', 'Catcher', 'First', 'Third']
   },
   {
     id: 3,
     name: 'Easton',
     playerNumber: 11,
-    image: DefaultPlayer,
+    image: Easton,
     positions: ['Catcher', 'Outfield']
   },
   {
     id: 4,
     name: 'Bron',
     playerNumber: 99,
-    image: DefaultPlayer,
+    image: Bron,
     positions: ['Second', 'Outfield']
   },
   {
     id: 5,
     name: 'Carter',
     playerNumber: 7,
-    image: DefaultPlayer,
+    image: Carter,
     positions: ['Catcher', 'Outfield']
   },
   {
     id: 6,
     name: 'Connor',
     playerNumber: 44,
-    image: DefaultPlayer,
-    positions: ['Pitcher', 'First']
+    image: Connor,
+    positions: ['Pitcher', 'First', 'Outfield']
   },
   {
     id: 7,
     name: 'Ethan',
     playerNumber: 13,
-    image: DefaultPlayer,
-    positions: []
+    image: Ethan,
+    positions: ['Pitcher', 'Outfield', 'Infield']
   },
   {
     id: 8,
     name: 'Holden',
     playerNumber: 66,
-    image: DefaultPlayer,
-    positions: []
+    image: Holden,
+    positions: ['Pitcher', 'Catcher', 'Outfield', 'Infield']
   },
   {
     id: 9,
     name: 'Jaxson',
     playerNumber: 24,
-    image: DefaultPlayer,
-    positions: ['Pitcher']
+    image: Jaxson,
+    positions: ['Pitcher', 'Infield', 'Outfield']
   },
   {
     id: 10,
     name: 'Kelton',
     playerNumber: 14,
-    image: DefaultPlayer,
-    positions: []
+    image: Kelton,
+    positions: ['Pitcher', 'Second', 'Outfield']
   },
   {
     id: 11,
     name: 'Peter',
     playerNumber: 17,
-    image: DefaultPlayer,
-    positions: []
+    image: Peter,
+    positions: ['Pitcher', 'Third', 'First']
   },
   {
     id: 12,
     name: 'Warner',
     playerNumber: 10,
-    image: DefaultPlayer,
-    positions: []
+    image: Warner,
+    positions: ['Pitcher', 'First', 'Outfield']
   }
 ]
